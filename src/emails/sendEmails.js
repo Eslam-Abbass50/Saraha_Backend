9b1ae77eb823ca9f086bbd0987fea896f47f17ab
Eslam-Abbass50 <eslamabbass50@gmail.com>
2025-08-22 23:31:52 +0300

    add email sendding

--- src/emails/sendEmails.js
+++ src/emails/sendEmails.js
@@ -1,21 +1,23 @@
- import nodemailer  from "nodemailer"
+import nodemailer from "nodemailer";
 import { html } from "./templetConfirmation.js";
-import  jwt  from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
-const SendEmail =async(option)=>{
-    const transporter = nodemailer.createTransport({
-service:"gmail",
-  auth: {
-       user: "noteappmearn26@gmail.com",
-    pass: "btde gfvv pjnw rtsd"
-  },
-});
-const token = jwt.sign({ email:option.email }, process.env.SECRET_KEY2, { expiresIn: "1h" });
+const SendEmail = async (option) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.MYEMAIL,
+      pass: process.env.pass,
+    },
+  });
+  const token = jwt.sign({ email: option.email }, process.env.SECRET_KEY2, {
+    expiresIn: "1h",
+  });
   const info = await transporter.sendMail({
-    from: ' noteappmearn26@gmail.com',
+    from: " noteappmearn26@gmail.com",
     to: option.email,
     subject: "Hello ✔",
-    html:html(token), // HTML body
-});
-}
-export default SendEmail
+    html: html(token), // HTML body
+  });
+};
+export default SendEmail;
